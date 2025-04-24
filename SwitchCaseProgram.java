8fa9e2383bc0db381c1674610d6c65f1f1a954fb
amita143-sudo <amitamanwatkar@gmail.com>
2025-04-24 15:32:54 +0530

    case peogram

---
+++ SwitchCaseProgram.java
@@ -0,0 +1,26 @@
+package revision_class;
+
+public class SwitchCaseProgram {
+
+	public static void main(String[] args) {
+		int a= 1;
+		int b=4;
+		switch(b)
+		{
+		case 1: System.out.println("addition");
+		break;
+		case 2: System.out.println("substraction");
+		break;
+		case 3: System.out.println("multiplication");
+		break;
+		case 4: System.out.println("division");
+		break;
+		default:
+		{
+			System.out.println("selection is wrong");
+		}
+		
+		}
+	}
+
+}
